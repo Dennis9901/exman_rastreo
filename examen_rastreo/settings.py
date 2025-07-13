c99b76af6628581947c39213170ea13745cf9bcf
dennis sanchez <dennissanchez1999@gmail.com>
2025-07-12 23:10:14 -0600

    Cambios

--- examen_rastreo/settings.py
+++ examen_rastreo/settings.py
@@ -2,7 +2,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'examen-rastreo-key'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
@@ -40,11 +40,9 @@ TEMPLATES = [{
 
 WSGI_APPLICATION = 'examen_rastreo.wsgi.application'
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 AUTH_PASSWORD_VALIDATORS = []
