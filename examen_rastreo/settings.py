6997a016fdc02c75397187c0c91acf0a15a08d12
dennis sanchez <dennissanchez1999@gmail.com>
2025-07-14 00:22:54 -0600

    Cambios

--- examen_rastreo/settings.py
+++ examen_rastreo/settings.py
@@ -40,9 +40,11 @@ TEMPLATES = [{
 
 WSGI_APPLICATION = 'examen_rastreo.wsgi.application'
 
-import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = []
